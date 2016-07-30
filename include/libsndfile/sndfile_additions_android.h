df7991fcf072d7f9bea212179434e87cb8464586
Louis M <prog@tioui.com>
2016-07-30 01:01:17 -0400

    Adding C libraries.

--- include/libsndfile/sndfile_additions_android.h
+++
@@ -1,8 +0,0 @@
-#ifndef AUDIO_ADDITIONS_ANDROID_H
-#define AUDIO_ADDITIONS_ANDROID_H
-#include <sndfile.h>
-#include <SDL.h>
-
-void setSndFileVirtualIo_RW(SF_VIRTUAL_IO *VirtualIO);
-
-#endif
--- link_libs.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-cd libs
-for f in * ; do ln -s $f android-${f} ; done
